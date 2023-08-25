447e81dc3484b68865ac678d45fd897a336f5303
Spencer <eklund.spencer@outlook.com>
2023-08-25 11:48:19 -0600

    utilizaed useState to fetch and display data

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
-// create your App component here
+import React, {useState, useEffect} from 'react';
+
+function App() {
+
+const [image, setImage] = useState(null);
+
+useEffect (() => {
+fetch('https://dog.ceo/api/breeds/image/random')
+.then(r => r.json())
+.then(data => { setImage(data.message)}
+)},
+ []);
+
+
+return (
+    <div>
+        {image ? (<img src={image} alt ="A Random Dog"></img>) :
+        ( <p>Loading... </p> )
+    }
+
+    </div>
+    )
+}
+
+export default App;
